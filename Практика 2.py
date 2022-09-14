82bb9d2693eff40170da7866ee6b6d3996b303d8
Aladyx <108027003+Aladyx@users.noreply.github.com>
2022-09-15 00:07:07 +0300

    Update Практика 2.py

--- Практика 2.py
+++ Практика 2.py
@@ -1,6 +1,6 @@
 import math
-x = float(input())
-y = float(input())
-z = float(input())
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+z = float(input("Введите z: "))
 s = (((2 * math.cos(x - (2 / 3))) / ((1 / 2) + (math.pow(math.sin(y), 2)))) * (1 + (math.pow(z, 2) / (3 - (math.pow(z, 2) / 5)))))
 print("s = {0:.6f}".format(s))
